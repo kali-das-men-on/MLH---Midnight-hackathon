604903ecb133186905b4f0f3723c74e01bcdb34a
Kalidas <kalidaspmenon@gmail.com>
2026-07-18 10:29:59 -0400

    frontend change fixing issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
   base: "/MLH---Midnight-hackathon/",
-
-  server: {
-    port: 5173,
-  },
+  server: { port: 5173 },
 });
